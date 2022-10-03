6d7d1c8e61038906c001dbafd50120fa3d48d9c9
Luna Nordbergh <luna.nordbergh2@gmail.com>
2022-10-03 12:09:38 +0200

    Added linux files
    Bash(.sh), pyhon3(.py)

---
+++ templates/linux/cs/compiler.bat
@@ -0,0 +1,2 @@
+"C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe" main.cs
+pause
---
+++ templates/linux/cs/main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class HelloWorld{    
+    static void Main(){
+
+
+
+        //This stops the program to close immediately after start.
+        Console.WriteLine("End of program Press Enter to Exit");
+        Console.ReadLine();
+    }
+}
